c39c09d5baa376bdfc4c3c84ad20258ce037757f
DmitMark <dmmarkushevski@gmail.com>
2023-06-23 14:18:24 +0300

    refactoring

--- app.py
+++ app.py
@@ -8,10 +8,10 @@ bot = telebot.TeleBot(TOKEN)
 
 @bot.message_handler(commands=['start', 'help'])
 def help(message):
-    text = f'Приветствую Вас (message.from_user.first_name)\n'\
+    text = f'Приветствую Вас {message.from_user.first_name} \n'\
            f'Чтобы начать работу, введите команду боту в следующем формате: \n<имя валюты> '\
            '< в какую валюту перевести >'\
-           '< количество переводимой валюты >'
+           '< количество переводимой валюты >'\
     '\nЧтобы увидеть список всех доступных валют,введите команду: /values'
     bot.reply_to(message,text)
 
@@ -27,13 +27,13 @@ def values(message: telebot.types.Message):
 @bot.message_handler(content_types=['text',])
 def convert(message: telebot.types.Message):
     try:
+        data_values = message.text.split(' ')
 
-        data_values = \
-            message.text.split(' ')
         if len(data_values) > 3:
             raise ConvertionException('Вы ввели слишком много параметров')
         elif len(data_values) < 3:
             raise ConvertionException('Вы ввели слишком мало параметров')
+
         quote, base, amount = data_values
         total_base = Converter.get_price(quote,base,amount)
     except ConvertionException as e:
@@ -43,6 +43,7 @@ def convert(message: telebot.types.Message):
     else:
         text = f'Цена <b>{amount}{quote} </b> в <b>{base}</b>-<b>{total_base}</b>'
         bot.send_message(message.chat.id, text, parse_mode='html')
+
 bot.polling(none_stop=True)
 
 
--- config.py
+++ config.py
@@ -2,5 +2,5 @@ TOKEN = '5877781772:AAEkgRBbqI3WSSvGtzxA29ZVj265UbCOdY8'
 keys = {
     'рубль': 'RUB',
     'евро': 'EUR',
-    'доллар США': 'USD'
+    'доллар': 'USD'
 }
